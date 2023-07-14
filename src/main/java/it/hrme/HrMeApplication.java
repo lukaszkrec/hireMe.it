6931fd16040a27412c74325544f684664b0f1c85
Łukasz Kręc <lukasz.krec@onet.pl>
2023-07-14 23:41:40 +0200

    Initial commit

---
+++ src/main/java/it/hrme/HrMeApplication.java
@@ -0,0 +1,15 @@
+package it.hrme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMeApplication.class, args);
+    }
+}
+
+
+
---
+++ src/test/java/it/hrme/HrMeApplicationTests.java
@@ -0,0 +1,10 @@
+package it.hrme;
+
+import org.junit.jupiter.api.Test;
+
+class HrMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
